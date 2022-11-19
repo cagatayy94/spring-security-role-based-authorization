947f98d92aa3f8ce9e73df30e7dea4c6651ddaff
cagatay yilmaz <mustafa.cagatay.yilmaz@gmail.com>
2022-11-20 00:27:30 +0300

    Implemented refresh token

--- src/main/java/com/example/springsecurity/entity/api/AppUserController.java
+++ src/main/java/com/example/springsecurity/entity/api/AppUserController.java
@@ -1,21 +1,36 @@
 package com.example.springsecurity.entity.api;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.example.springsecurity.entity.AppUser;
 import com.example.springsecurity.entity.Role;
 import com.example.springsecurity.entity.business.AppUserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.net.URI;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static java.util.Arrays.stream;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
+@Slf4j
 public class AppUserController {
     private AppUserService userService;
 
@@ -45,6 +60,56 @@ public class AppUserController {
     public ResponseEntity<?> addRoleToUser(@RequestBody RoleToUserForm form){
         userService.addRoleToUser(form.username, form.rolename);
         return ResponseEntity.ok().build();
+    }
+    @PostMapping("/token/refresh")
+    public void addRoleToUser(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        String authorizationHeader = request.getHeader(AUTHORIZATION);
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")){
+            try {
+                String token = authorizationHeader.substring("Bearer ".length());
+                Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
+
+                JWTVerifier verifier = JWT.require(algorithm).build();
+                DecodedJWT decodedJWT = verifier.verify(token);
+
+                String username = decodedJWT.getSubject();
+                AppUser appUser = userService.getUser(username);
+
+                String accessToken = JWT.create()
+                        .withSubject(appUser.getUsername())
+                        .withExpiresAt(new Date(System.currentTimeMillis() + 10 * 60 * 1000))
+                        .withIssuer(request.getRequestURL().toString())
+                        .withClaim("roles", appUser.getRoles().stream().map(Role::getName).collect(Collectors.toList()))
+                        .sign(algorithm);
+
+                String refreshToken = JWT.create()
+                        .withSubject(appUser.getUsername())
+                        .withExpiresAt(new Date(System.currentTimeMillis() + 30 * 60 * 1000))
+                        .withIssuer(request.getRequestURL().toString())
+                        .sign(algorithm);
+
+                Map<String, String> tokens = new HashMap<>();
+
+                tokens.put("access_token", accessToken);
+                tokens.put("refresh_token", refreshToken);
+                response.setContentType(APPLICATION_JSON_VALUE);
+
+                new ObjectMapper().writeValue(response.getOutputStream(), tokens);
+
+
+            }catch (Exception e){
+                log.error("Error logging in {}", e.getMessage());
+                response.setHeader("error", e.getMessage());
+                response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+                Map<String, String> error = new HashMap<>();
+                error.put("error_message", e.getMessage());
+                response.setContentType(APPLICATION_JSON_VALUE);
+                new ObjectMapper().writeValue(response.getOutputStream(), error);
+            }
+        }else {
+            throw new RuntimeException("refresh token is missing");
+        }
     }
 
     @Data
--- src/main/java/com/example/springsecurity/entity/security/filter/CustomAuthorizationFilter.java
+++ src/main/java/com/example/springsecurity/entity/security/filter/CustomAuthorizationFilter.java
@@ -29,14 +29,14 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 public class CustomAuthorizationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        if (request.getServletPath().equals("/api/login")){
+        if (request.getServletPath().equals("/api/login") || request.getServletPath().equals("/api/token/refresh")){
             filterChain.doFilter(request, response);
         }
 
         String authorizationHeader = request.getHeader(AUTHORIZATION);
         if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")){
             try {
-                String token = authorizationHeader.substring("Bearer".length());
+                String token = authorizationHeader.substring("Bearer ".length());
                 Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
 
                 JWTVerifier verifier = JWT.require(algorithm).build();
